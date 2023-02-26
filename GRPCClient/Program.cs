d2376022d08efc92c76704e9f62ea5a4611b49bd
pavelapk <pepdj@mail.ru>
2023-02-26 15:06:53 +0700

    init

---
+++ GRPCClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GRPCClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7054");
+var client = new Byeer.ByeerClient(channel);
+
+var response = await client.SayByeAsync(
+    new ByeRequest {
+        Name = "aboba"
+    }
+);
+
+Console.WriteLine(response?.Message);
+
+Console.ReadLine();
---
+++ GrpcService2/Services/ByeerService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace GrpcService2.Services;
+
+public class ByeerService : Byeer.ByeerBase {
+    private readonly Greeter.GreeterClient _client;
+
+    public ByeerService() {
+        var channel = GrpcChannel.ForAddress("https://localhost:7078");
+        _client = new Greeter.GreeterClient(channel);
+    }
+
+    public override async Task<ByeReply> SayBye(ByeRequest request, ServerCallContext context) {
+        var helloReply = await _client.SayHelloAsync(new HelloRequest {
+            Name = request.Name
+        });
+        return new ByeReply {
+            Message = $"Bye {helloReply.Message}"
+        };
+    }
+}
